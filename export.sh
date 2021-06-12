86d18c3aad4015b0c0a7bf7f82ffdc44294b91cd
Markus Linnala <markus.linnala@gmail.com>
2021-06-12 16:58:10 +0300

    export.sh: Make it actually work

--- export.sh
+++ export.sh
@@ -4,15 +4,18 @@
 # mkdir -p "$DIR"/var/lib/selinux/targeted
 # make DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR=tmp/install 'SEMODULE=/usr/sbin/semodule -v -p '"$DIR"' -X 100 ' load
 
-( $# )
+(( $# ))
 
-DIR="$1"
+DIR="$(readlink -f -- "$1")"
 SEMODULE=(semodule -p "$DIR")
 
+rmfiles=()
 args=()
 for a in $("${SEMODULE[@]}" -l); do
+    rmfiles+=("$a".cil)
     args+=(--cil --extract="$a")
 done
 mkdir -p export
 cd export
+rm -f -- "${rmfiles[@]}"
 "${SEMODULE[@]}" "${args[@]}"
