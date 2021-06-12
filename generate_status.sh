598e2989f406c41f8de76dcb4b10fc9660e7d87e
Markus Linnala <markus.linnala@gmail.com>
2021-06-12 15:42:39 +0300

    generate_status.sh: grep errors if no hits

--- generate_status.sh
+++ generate_status.sh
@@ -12,7 +12,9 @@ if [ ! -f "${logs[0]}" ]; then
     exit 0
 fi
 
+set +e
 grep -El '^# found:' "$SD"/*"$EXT" > "$D"/found.txt
 grep -El '^# no:' "$SD"/*"$EXT" > "$D"/no.txt
 grep -El '^# some:' "$SD"/*"$EXT" > "$D"/some.txt
-grep -Fvf "$D"/no.txt "$D"/found.txt | grep -Fvf "$D"/some.txt || :
+grep -Fvf "$D"/no.txt "$D"/found.txt | grep -Fvf "$D"/some.txt
+exit 0
