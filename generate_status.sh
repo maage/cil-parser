843fa6cbf90dca49234b79d2a38a8b3481f32da1
Markus Linnala <markus.linnala@gmail.com>
2021-06-12 15:25:41 +0300

    generate_status.sh: guard agains situation where there is logs yet

--- generate_status.sh
+++ generate_status.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-D=split_lines
+set -epu -o pipefail
 
-grep -El '^# found:' tmp/*log > "$D"/found.txt
-grep -El '^# no:' tmp/*.log > "$D"/no.txt
-grep -El '^# some:' tmp/*.log > "$D"/some.txt
+D=split_lines
+SD=tmp
+EXT=.log
+
+# Is there any logs to analyze
+logs=("$SD"/*"$EXT")
+if [ ! -f "${logs[0]}" ]; then
+    exit 0
+fi
+
+grep -El '^# found:' "$SD"/*"$EXT" > "$D"/found.txt
+grep -El '^# no:' "$SD"/*"$EXT" > "$D"/no.txt
+grep -El '^# some:' "$SD"/*"$EXT" > "$D"/some.txt
 grep -Fvf "$D"/no.txt "$D"/found.txt | grep -Fvf "$D"/some.txt || :
