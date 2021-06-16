694d7a7f1820ef5fb9fdf2272cc471a9826ffe2d
Markus Linnala <markus.linnala@gmail.com>
2021-06-16 21:19:58 +0300

    refpolicy_split.sh: Add ok/err files to keep track successes and failures

--- refpolicy_split.sh
+++ refpolicy_split.sh
@@ -7,10 +7,12 @@ D="$1"
 
 f=()
 while IFS='' read -d '' -r a && [ "$a" ]; do
-    if (( ${#f[@]} >= 10 )); then
-        break
-    fi
+    ok=export/"$(basename "$a")".ok
+    err=export/"$(basename "$a")".err
+    [ ! -f "$ok" ] || continue
+    [ ! -f "$err" ] || continue
     rm -rf split_lines
     ./split_te.sh "$a"
-    make -j"$(nproc)" -k || make -j"$(nproc)"
+    make -j"$(nproc)" -k || make -j"$(nproc)"  # || { touch "$err"; continue; }
+    touch "$ok"
 done < <(find "$D" -name '*.te' -type f -print0 | shuf -z)
