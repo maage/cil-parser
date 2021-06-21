76a6fc2a8211392db9ce5d32f9e6dbd9bd1ff5d0
Markus Linnala <markus.linnala@gmail.com>
2021-06-21 09:59:35 +0300

    export-modules.sh: only update changed modules
    Previously removed all.

--- export-modules.sh
+++ export-modules.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm -rf export
-mkdir -p export
-pushd export
+mkdir -p export/tmp
+
+pushd export/tmp
+
 args=() # --verbose)
 for a in $(sudo semodule -l); do
     args+=(--cil --extract="$a")
@@ -11,4 +12,18 @@ done
 sudo semodule "${args[@]}"
 
 popd
+
 sudo chown -R "$USER": export
+
+pushd export/tmp
+for a in *.cil; do
+    [ -f "$a" ] || continue
+    if [ -f ../"$a" ] && cmp -s "$a" ../"$a"; then
+        :
+    else
+        mv -- "$a" ..
+    fi
+done
+popd
+
+rm -rf export/tmp
