b55c4a6d1fad08a071f8ea81958c3c769ce980a2
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-21 14:07:06 +0300

    export.sh: Simpler method to do same thing

--- export.sh
+++ export.sh
@@ -4,33 +4,29 @@
 # mkdir -p "$DIR"/var/lib/selinux/targeted
 # make DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR=tmp/install 'SEMODULE=/usr/sbin/semodule -v -p '"$DIR"' -X 100 ' load
 
+set -epu -o pipefail
+
 (( $# ))
 
 DIR="$(readlink -f -- "$1")"
 SEMODULE=(semodule -p "$DIR")
 
-mkdir -p export
-cd export
+mkdir -p export/tmp
+pushd export/tmp
 
 oldfiles=()
 args=()
 for a in $("${SEMODULE[@]}" -l); do
-    if [ -f "$a".cil ]; then
-        oldfiles+=("$a".cil)
-        mv -- "$a".cil "$a".cil.old
-    fi
     args+=(--cil --extract="$a")
 done
 "${SEMODULE[@]}" "${args[@]}"
-olddel=()
-for a in "${oldfiles[@]}"; do
-    if [ -f "$a" ] && cmp -s "$a".old "$a"; then
-        # Use old file if no change
-        mv -- "$a".old "$a"
+for a in *.cil; do
+    [ -f "$a" ] || continue
+    if [ -f "$a" ] && cmp -s "$a" ../"$a"; then
+        :
     else
-        olddel+=("$a".old)
+        mv -- "$a" ../"$a"
     fi
 done
-if (( ${#olddel[@]} )); then
-    rm -f -- "${olddel[@]}"
-fi
+popd
+rm -rf -- export/tmp
