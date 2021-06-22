739abbb4fe5fcfd053d5f62933f7558cbc670147
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-22 16:38:57 +0300

    export.sh: better help how to generate

--- export.sh
+++ export.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Directory can be made running somehing like this:
+# export DIR="$(pwd)/tmp/install
+# printf "DIR=%q\n" "$DIR"
 # mkdir -p "$DIR"/var/lib/selinux/targeted
-# make DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR=tmp/install 'SEMODULE=/usr/sbin/semodule -v -p '"$DIR"' -X 100 ' load
+# make -j$(nproc) DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR=tmp/install 'SEMODULE=/usr/sbin/semodule -v -p '"$DIR"' -X 100 ' load
 
 set -epu -o pipefail
 
