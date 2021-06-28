818119d3304e2a1b8e4fb5dd6fe6e1348869e739
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-28 10:49:54 +0300

    refpolicy_split.sh: really use external policy

--- refpolicy_split.sh
+++ refpolicy_split.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
+# To ensure split_te works for all kinds of policies, this iterates over
+# refpolicy variant for example:
+# - SELinuxProject/refpolicy
+# - fedora-selinux/selinux-policy
+# If pass is fully done, then we can deduct our parsing is reasonable
+# fleshed out.
 
 set -epux -o pipefail
 
 (( $# ))
 D="$1"
+
+export DESTDIR="$(readlink -f -- "$D"/tmp/install)"
+mkdir -p "$DESTDIR"/var/lib/selinux/targeted
+
+make_refpolicy() {
+    make DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR="$DESTDIR" 'SEMODULE=/usr/sbin/semodule -v -p '"$DESTDIR"' -X 100 ' "$@"
+}
+
+if [ ! -f "$DESTDIR"/usr/share/selinux/devel/Makefile ]; then
+    pushd "$D"
+    make_refpolicy conf
+    make_refpolicy -j$(nproc) load
+    make_refpolicy NAME=devel install-headers
+    cp /usr/share/selinux/devel/Makefile "$DESTDIR"/usr/share/selinux/devel/Makefile
+    sed -ri 's,(SHAREDIR :=).*,\1 '"$DESTDIR"/usr/share/selinux',' "$DESTDIR"/usr/share/selinux/devel/Makefile
+    popd
+fi
 
 f=()
 while IFS='' read -d '' -r a && [ "$a" ]; do
@@ -13,11 +36,11 @@ while IFS='' read -d '' -r a && [ "$a" ]; do
     [ ! -f "$err" ] || continue
     rm -rf split_lines
     ./split_te.sh "$a"
-    make tmp/all_interfaces.conf
+    make DESTDIR="$DESTDIR" tmp/all_interfaces.conf
     if [ -f "$D"/tmp/all_interfaces.conf ]; then
         cp "$D"/tmp/all_interfaces.conf tmp/all_interfaces.conf
         touch tmp/all_interfaces.conf
     fi
-    make -j"$(nproc)" -k || make -j"$(nproc)"  # || { touch "$err"; continue; }
+    make DESTDIR="$DESTDIR" -j"$(nproc)" -k || make DESTDIR="$DESTDIR" -j"$(nproc)"  # || { touch "$err"; continue; }
     touch "$ok"
 done < <(find "$D" -name '*.te' -type f -print0 | shuf -z)
