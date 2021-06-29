53aec944e3e789f7890ef870944047dd60352760
Markus Linnala <Markus.Linnala@cybercom.com>
2021-06-29 11:57:54 +0300

    refpolicy_split.sh: better error handling

--- refpolicy_split.sh
+++ refpolicy_split.sh
@@ -46,21 +46,30 @@ s/^[# ]*?(WERROR *=).*/\1 y/;
 fi
 
 rm -rf split_lines
-make DESTDIR="$DESTDIR" -j"$(nproc)" -k || make DESTDIR="$DESTDIR" -j"$(nproc)"
+make DESTDIR="$DESTDIR" -j"$(nproc)" -k
 
 f=()
 while IFS='' read -d '' -r a && [ "$a" ]; do
+
     ok=export/"$(basename "$a")".ok
     err=export/"$(basename "$a")".err
+
     [ ! -f "$ok" ] || continue
     [ ! -f "$err" ] || continue
-    rm -rf split_lines
-    ./split_te.sh "$a"
-    make DESTDIR="$DESTDIR" tmp/all_interfaces.conf
-    if [ -f "$D"/tmp/all_interfaces.conf ]; then
-        cp "$D"/tmp/all_interfaces.conf tmp/all_interfaces.conf
-        touch tmp/all_interfaces.conf
+
+    rm -rf split_lines tmp/all_interfaces.conf
+
+    declare -i rc=0
+    ./split_te.sh "$a" || rc=$?
+
+    if (( ! rc )); then
+        make DESTDIR="$DESTDIR" -j"$(nproc)" -k || rc=$?
     fi
-    make DESTDIR="$DESTDIR" -j"$(nproc)" -k || make DESTDIR="$DESTDIR" -j"$(nproc)" || { touch "$err"; continue; }
-    touch "$ok"
+
+    if (( rc )); then
+        touch "$err"
+    else
+        touch "$ok"
+    fi
+
 done < <(find "$D" -name '*.te' -type f -print0 | shuf -z)
