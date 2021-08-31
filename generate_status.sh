79dff6f884fe34c4fa40bfdc8415d5b22942caaa
Markus Linnala <Markus.Linnala@cybercom.com>
2021-08-31 16:20:53 +0300

    generate_status.sh: Use while loop and printf more

--- generate_status.sh
+++ generate_status.sh
@@ -10,9 +10,7 @@ set +e
 grep -El '^# found:' -- "$@" > "$D"/found.txt
 grep -El '^# no:' -- "$@" > "$D"/no.txt
 grep -El '^# some:' -- "$@" > "$D"/some.txt
-for a in $(grep -Fvf "$D"/no.txt "$D"/found.txt | grep -Fvf "$D"/some.txt); do
-    printf "# file: %s\n" "$a"
-    < "$a"
-    echo
-done
+while read -r a; do
+    printf "# file: %s\n%s\n" "$a" "$(<"$a")"
+done < <(grep -Fvf "$D"/no.txt "$D"/found.txt | grep -Fvf "$D"/some.txt)
 exit 0
