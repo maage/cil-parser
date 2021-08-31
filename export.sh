ec3c0a466b81ff5210e770412bb3207eae160d2d
Markus Linnala <Markus.Linnala@cybercom.com>
2021-08-31 14:05:33 +0300

    export.sh: better way to run

--- export.sh
+++ export.sh
@@ -4,14 +4,20 @@
 # export DESTDIR="$(pwd)"/tmp/install
 # printf "export DESTDIR=%q\n" "$DESTDIR"
 # mkdir -p "$DESTDIR"/var/lib/selinux/targeted
-# make DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR="$DESTDIR" 'SEMODULE=/usr/sbin/semodule -v -p '"$DESTDIR"' -X 100 ' conf
+## make DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR="$DESTDIR" 'SEMODULE=/usr/sbin/semodule -v -p '"$DESTDIR"' -X 100 ' conf
 # make -j$(nproc) DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR="$DESTDIR" 'SEMODULE=/usr/sbin/semodule -v -p '"$DESTDIR"' -X 100 ' load
+# make DESTDIR="$DESTDIR" 'SEMODULE=/usr/sbin/semodule -v -p '"$DESTDIR"' -X 100 ' NAME=devel install-headers
+# cp /usr/share/selinux/devel/Makefile "$DESTDIR"/usr/share/selinux/devel/Makefile
+# sed -ri 's,(SHAREDIR :=).*,\1 '"$DESTDIR"/usr/share/selinux',' "$DESTDIR"/usr/share/selinux/devel/Makefile
 
 set -epu -o pipefail
 
-(( $# ))
+if [ ! "${DESTDIR:-}" ]; then
+    (( $# ))
+    DESTDIR="$(readlink -f -- "$1")"
+    shift
+fi
 
-DESTDIR="$(readlink -f -- "$1")"
 SEMODULE=(semodule -p "$DESTDIR")
 
 mkdir -p export/tmp
@@ -22,7 +28,9 @@ args=()
 for a in $("${SEMODULE[@]}" -l); do
     args+=(--cil --extract="$a")
 done
+
 "${SEMODULE[@]}" "${args[@]}"
+
 for a in *.cil; do
     [ -f "$a" ] || continue
     if [ -f "$a" ] && cmp -s "$a" ../"$a"; then
@@ -31,5 +39,6 @@ for a in *.cil; do
         mv -- "$a" ../"$a"
     fi
 done
+
 popd
 rm -rf -- export/tmp
